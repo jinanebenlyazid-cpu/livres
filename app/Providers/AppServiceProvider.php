85bbf1a842f755ead7bb0b1f13527ac2db3b6a54
iman <aajnahiman@gmail.com>
2026-05-19 11:17:35 +0100

    fix https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,9 +5,13 @@ namespace App\Providers;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
+
+
+
+
     /**
      * Register any application services.
      */
@@ -19,9 +23,13 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+     public function boot(): void
     {
         Paginator::useBootstrapFive();
         Schema::defaultStringLength(191);
+
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
